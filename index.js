6a2e7c1ad3219a57c1e09979b8241bf8c383273d
bielalexandres <gabrielalexandredasilva86@gmail.com>
2022-07-09 17:04:07 -0300

    erased ->app.set('views', path.join(__dirname, 'views'))

--- index.js
+++ index.js
@@ -16,7 +16,7 @@
 })
 
 
-app.set('views', path.join(__dirname, 'views'))
+
 app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname,'public')))
 app.use(bodyParser.urlencoded({ extended: true }))

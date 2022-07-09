286bf2976f1e40e8a63a9ea1f21228420aff144a
bielalexandres <gabrielalexandredasilva86@gmail.com>
2022-07-09 17:07:43 -0300

    erased papp.set path.join(__dirname,

--- index.js
+++ index.js
@@ -18,7 +18,7 @@
 
 
 app.set('view engine', 'ejs')
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 
 

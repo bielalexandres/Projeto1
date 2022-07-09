152f8066e650197dad8d0f1057d4f742745cd9a6
bielalexandres <gabrielalexandredasilva86@gmail.com>
2022-07-09 16:39:18 -0300

    include (path.resolve(_dirname and app.use('/admin', (req, res, next) => {     if(req.hostname === 'localhost') {         next()     } else {         res.send('Not allowed')     } })
    app.set('views', path.join(__dirname, 'views'))
    app.set('view engine', 'ejs')
    app.use(express.static(path.join(__dirname,'public')))
    app.use(bodyParser.urlencoded({ extended: true }))

--- index.js
+++ index.js
@@ -3,13 +3,24 @@
  const bodyParser = require('body-parser')
 
  const sqlite = require('sqlite')
- const dbConnection = sqlite.open('banco.sqlite', { Promise })
+ const dbConnection = sqlite.open (path.resolve(__dirname,'banco.sqlite', { Promise }))
 
  const port = process.env.PORT || 3000
-    
- app.set ('view engine','ejs')
- app.use (express.static('public'))
- app.use(bodyParser.urlencoded({ extended: true}) )
+
+ app.use('/admin', (req, res, next) => {
+    if(req.hostname === 'localhost') {
+        next()
+    } else {
+        res.send('Not allowed')
+    }
+})
+
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.use(express.static(path.join(__dirname,'public')))
+app.use(bodyParser.urlencoded({ extended: true }))
+
 
  app.get('/', async(request, response) => {
     const db = await dbConnection
